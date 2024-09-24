32428750ae0bebe70bb9f4dfabb85755364a23de
Conrad Christian <cchristian5@my.bcit.ca>
2024-09-24 11:36:45 -0700

    build mvc

---
+++ expense_transactions/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace expense_transactions.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
