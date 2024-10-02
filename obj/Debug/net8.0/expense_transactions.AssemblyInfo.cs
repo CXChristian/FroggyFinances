02661c75d13652b2744c0815963778aa8c2ada3b
Conrad Christian <conradchristian812@gmail.com>
2024-10-01 23:45:40 -0700

    Create authentication to see buckets page

--- obj/Debug/net8.0/expense_transactions.AssemblyInfo.cs
+++ obj/Debug/net8.0/expense_transactions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568cc0d8dcfb9c00046a8032581fe410fd0215a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bb45443cdb37099c5584d1b950b1eca696053f")]
 [assembly: System.Reflection.AssemblyProductAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
