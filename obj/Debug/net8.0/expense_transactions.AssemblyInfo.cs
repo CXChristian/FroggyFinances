985e2522492a07ab5e8055e4e299b5b3d0be3cf1
Conrad Christian <cchristian5@my.bcit.ca>
2024-10-03 14:36:04 -0700

    Remove login and register page bloat

--- obj/Debug/net8.0/expense_transactions.AssemblyInfo.cs
+++ obj/Debug/net8.0/expense_transactions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0f76272d7ec92bfe49e9761e3b7ec5e6d8e9bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56460a72053b3366d74d4f5831eeec0382aeb924")]
 [assembly: System.Reflection.AssemblyProductAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
