ba33a7b57647ecae0fa24798ca27063becd1db56
mikaem <mkmanaligod@gmail.com>
2024-10-03 19:35:46 -0700

    .

--- obj/Debug/net8.0/expense_transactions.AssemblyInfo.cs
+++ obj/Debug/net8.0/expense_transactions.AssemblyInfo.cs
@@ -16,6 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1744095674fd680a84a124f7d5343df348aee55d")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1baec91039a95de9599b28950d62d9c8c97849")]
@@ -23,6 +24,9 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1744095674fd680a84a124f7d5343df348aee55d")]
 >>>>>>> e845c05 (.)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb9ff308bf29aabaa389b7d4780c26409715048")]
+>>>>>>> e10ba70 (.)
 [assembly: System.Reflection.AssemblyProductAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expense_transactions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
