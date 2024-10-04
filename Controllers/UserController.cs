6282c1e1c25e5b5f7316228e26e6ef6d21cd5f97
Conrad Christian <conradchristian812@gmail.com>
2024-10-04 05:22:27 -0700

    Add approve functionality to user manger page

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using expense_transactions.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace expense_transactions.Controllers;
 
@@ -21,4 +22,19 @@ public class UserController : Controller
 
         return View(users);
     }
+
+    public async Task<IActionResult> Approve(string id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.IsAdminApproved = true;
+            _context.Users?.Update(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
 }
